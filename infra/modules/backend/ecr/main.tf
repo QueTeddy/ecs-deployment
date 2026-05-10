e41488e9b6a8ba865277163644a8a94292d3b713
james.teddy <ebhamenteddyjr@gmail.com>
2026-05-11 00:16:13 +0100

    fixes: remove '-service' suffix from ECR repository name in main.tf

--- infra/modules/backend/ecr/main.tf
+++ infra/modules/backend/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr_repository" {
   for_each = toset(var.ECR_REPOSITORIES)
-  name = lower("${var.ENV}-${var.PROJECT_NAME}-${each.key}-service")
+  name = lower("${var.ENV}-${var.PROJECT_NAME}-${each.key}")
   tags = var.COMMON_TAGS
 }
