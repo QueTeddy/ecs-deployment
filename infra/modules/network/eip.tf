3c685324cded4995c594614c0deb34989dacadbd
james.teddy <ebhamenteddyjr@gmail.com>
2026-05-10 17:07:17 +0100

    fixes: fixing the naming

--- infra/modules/network/eip.tf
+++ infra/modules/network/eip.tf
@@ -1,4 +1,4 @@
 resource "aws_eip" "nateip" {
   domain = "vpc"
-  tags   = merge(var.COMMON_TAGS, tomap({"Name"= format("${var.ENV}-${var.PROJECT_NAME}-eip-%s", element(var.azs, count.index))}))
+  tags   = merge(var.COMMON_TAGS, tomap({"Name"= format("${var.ENV}-${var.PROJECT_NAME}-eip-%s")}))
 }
--- infra/modules/network/internet_gateway.tf
+++ infra/modules/network/internet_gateway.tf
@@ -1,7 +1,6 @@
 resource "aws_internet_gateway" "ecs_deployment" {
-  count    = "${length(var.public_subnets) > 0 ? 1 : 0}"
   vpc_id   = aws_vpc.ecs_deployment.id
-  tags     = merge(var.COMMON_TAGS, tomap({"Name"= format("${var.ENV}-${var.PROJECT_NAME}-igw-%s", element(var.azs, count.index))}))
+  tags     = merge(var.COMMON_TAGS, tomap({"Name"= format("${var.ENV}-${var.PROJECT_NAME}-igw-%s")}))
 }
 
 
