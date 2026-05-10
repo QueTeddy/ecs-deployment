a1f3768337a16d524483f99f5b3c9b355c11bd33
james.teddy <ebhamenteddyjr@gmail.com>
2026-05-10 17:43:57 +0100

    fixes: correct route_table_id reference in private_nat_gateway resource

--- infra/modules/network/route_tables.tf
+++ infra/modules/network/route_tables.tf
@@ -9,7 +9,7 @@ resource "aws_route_table" "private" {
 
 resource "aws_route" "private_nat_gateway" {
   count                  = length(var.azs)
-  route_table_id         = aws_route_table.private.id
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.natgw.id
 }
